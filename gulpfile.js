3259cc2c81a6a213a66a33570ee2bf15b86e0af6
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-01-31 12:00:04 +0530

    Initial push with bower components/npm components/gulp tasks added and dependencies installed.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// When creating new project and want to use this process, first install gulp dependencies via terminal (npm install --save-dev gulp gulp-coffee gulp-sass) within new project folder. Then paste this code (FOLLOW SAME FOLDER STRUCTURE AS HERE) within gulpfile.js
+// BOWER - Once above process is completed, within terminal and in root directory of new project execute 'bower init' -> 'bower install --save-dev'. Then instal/search for component needed (ie. jquery/normalize.css/animate.css/bootstrap.css etc)
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
+
+// Task for CoffeeScript
+gulp.task('coffee', function() {
+  gulp.src('./assets/js/coffee/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./assets/js/'))
+});
+// Task for watching CoffeeScript Folder
+gulp.watch('./assets/js/coffee/*.coffee', ['coffee']);
+
+// Task for Sass
+gulp.task('sass', function () {
+  gulp.src('./assets/css/src/brain.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css/'));
+});
+// Task for watching Sass folder
+gulp.watch('./assets/css/src/*.scss', ['sass']);
+
+// Task for initializing gulp processes (defined within the array) by simply entering 'gulp' in terminal
+gulp.task('default', ['coffee', 'sass']);
